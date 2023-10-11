2a489021f536109dd0628b9f4dfb11efbcde4b01
fl0cko <fl0cko@web.de>
2023-10-11 16:08:13 +0200

    Test

--- ESP32-Code/src/main.cpp
+++ ESP32-Code/src/main.cpp
@@ -1,5 +1,19 @@
 #include <Arduino.h>
 
+
+#define MOISTURE_SENSOR1_PIN 4
+#define MOISTURE_SENSOR2_PIN 5
+
+#define WATER_LEVEL_SENSOR_PIN 6
+
+#define PUMP_ENABEL_PIN 7
+
+#define SOLENOID1_PIN 15
+#define SOLENOID2_PIN 16
+#define SOLENOID3_PIN 17
+#define SOLENOID4_PIN 18
+
+
 // put function declarations here:
 int myFunction(int, int);
 
