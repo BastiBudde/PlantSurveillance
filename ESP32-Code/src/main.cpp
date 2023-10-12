639126c2b554d70ab34c396a7e68522c06df65e3
fl0cko <fl0cko@web.de>
2023-10-12 20:16:28 +0200

    Added functonality for usage of both cores

--- ESP32-Code/src/main.cpp
+++ ESP32-Code/src/main.cpp
@@ -14,11 +14,48 @@
 #define SOLENOID3_PIN 17
 #define SOLENOID4_PIN 18
 
+TaskHandle_t appCommunicationTask;
+TaskHandle_t plantSurveillanceTask;
+
 
 // put function declarations here:
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  pinMode(PUMP_ENABEL_PIN, OUTPUT);
+  pinMode(SOLENOID1_PIN, OUTPUT);
+  pinMode(SOLENOID2_PIN, OUTPUT);
+  pinMode(SOLENOID3_PIN, OUTPUT);
+  pinMode(SOLENOID4_PIN, OUTPUT);
+
+  xTaskCreatePinnedToCore(
+    appCommunicationCode,   /* Task function. */
+    "appCommunications",     /* name of task. */
+    10000,       /* Stack size of task */
+    NULL,        /* parameter of the task */
+    1,           /* priority of the task */
+    &appCommunicationTask,      /* Task handle to keep track of created task */
+    0);          /* pin task to core 0 */ 
+
+  xTaskCreatePinnedToCore(
+    plantSurveillanceCode,   /* Task function. */
+    "PlantSurveillance",     /* name of task. */
+    10000,       /* Stack size of task */
+    NULL,        /* parameter of the task */
+    1,           /* priority of the task */
+    &plantSurveillanceTask,      /* Task handle to keep track of created task */
+    1);          /* pin task to core 1 */ 
+}
+
+void appCommunicationCode(void *)
+{
+  return;
+}
+
+void plantSurveillanceCode(void *)
+{
+  return;
 }
 
 void loop() {
